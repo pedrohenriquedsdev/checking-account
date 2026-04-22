588951277a41e2d0cec956280942a0a64be04844
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-04-22 15:46:20 -0300

    refactor: split code into separate .cs files

---
+++ ContaCorrente.ConsoleApp/TelaPrincipal.cs
@@ -0,0 +1,100 @@
+namespace ContaCorrente
+{
+    class TelaPrincipal //Aprensentação e Interface com o Usuário
+    {
+        public string? ApresentarOpcoesDoMenu(ContaCorrente contaAcessada)
+        {
+            Console.Clear();
+
+            Console.WriteLine("MENU DE OPÇÕES");
+
+            Console.WriteLine("==========================");
+            Console.WriteLine($"Conta corrente #{contaAcessada.numeroIndentificacao} de {contaAcessada.titular}");
+            Console.WriteLine("==========================");
+            Console.WriteLine("1 - Sacar");
+            Console.WriteLine("2 - Depositar");
+            Console.WriteLine("3 - Transferência");
+            Console.WriteLine("4 - Consultar Saldo");
+            Console.WriteLine("S - Sair");
+            string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+            return opcaoMenu;
+        }
+
+        public void ApresentarOperacaoDeSaque(ContaCorrente contaAcessada)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Digite o valor que deseja sacar (R$): ");
+            decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+
+            bool conseguiuSacar = contaAcessada.Sacar(valorSaque);
+
+            if (!conseguiuSacar)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("O valor do limite de débito foi ultrapassado!");
+            }
+
+            else
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("O valor foi sacado com sucesso!");
+            }
+
+            Console.WriteLine("-----------------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+
+        public void ApresentarOperacaoDeDeposito(ContaCorrente contaAcessada)
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Digite o valor que deseja depositar (R$): ");
+            decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+
+            contaAcessada.Depositar(valorDeposito);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("O valor foi depositado com sucesso!");
+            Console.WriteLine("-----------------------------------");
+            Console.Write("Digite ENTER para continuar");
+            Console.ReadLine();
+        }
+
+        public void ApresentarOperacaoDeTransferencia(ContaCorrente contaAcessada, ContaCorrente contaDestino)
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Digite o valor que deseja transferir (R$): ");
+            decimal valorTransferencia = Convert.ToDecimal(Console.ReadLine());
+
+            bool conseguiuTransferir = contaAcessada.TransferirPara(contaDestino, valorTransferencia);
+
+            if (!conseguiuTransferir)
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine($"Não foi possível transferir o valor de R${valorTransferencia}");
+            }
+
+            else
+            {
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine($"O valor de R${valorTransferencia} foi transferido com sucesso!");
+            }
+
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+
+        public void ApresentarOperacaoObterSaldo(ContaCorrente contaAcessada)
+        {
+            decimal saldo = contaAcessada.ObterSaldo();
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"O saldo da conta é de: R${saldo}");
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
